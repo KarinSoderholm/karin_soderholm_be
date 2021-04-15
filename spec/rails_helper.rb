e1957adc017b7e85eac4a3094f5b2dd1082d3474
George Soderholm <georgesoderholm@gmail.com>
2021-04-15 16:42:51 -0600

    Add simple cov, shoulda matchers, capybara to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
+
+SimpleCov.start 'rails'
+SimpleCov.add_filter ["app/channels", "app/helpers", "app/mailers", "app/jobs", "app/models"]
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -38,7 +42,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
+  config.render_views = true 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
