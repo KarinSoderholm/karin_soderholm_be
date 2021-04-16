870bea70031ba2f83df2821a73347806123f5611
George Soderholm <georgesoderholm@gmail.com>
2021-04-15 22:12:13 -0600

    Remove relationships

--- spec/models/artshow_artwork_spec.rb
+++ spec/models/artshow_artwork_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ArtshowArtwork, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :artwork }
+    it { should belong_to :artshow }
+  end
 end
