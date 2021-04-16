0a4325f8ede4d56150d8d10e09f98dc5052f4fa8
George Soderholm <georgesoderholm@gmail.com>
2021-04-15 21:29:36 -0600

    Add spec skeleton files

---
+++ spec/routing/fabrics_routing_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe FabricsController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(get: "/fabrics").to route_to("fabrics#index")
+    end
+
+    it "routes to #show" do
+      expect(get: "/fabrics/1").to route_to("fabrics#show", id: "1")
+    end
+
+
+    it "routes to #create" do
+      expect(post: "/fabrics").to route_to("fabrics#create")
+    end
+
+    it "routes to #update via PUT" do
+      expect(put: "/fabrics/1").to route_to("fabrics#update", id: "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/fabrics/1").to route_to("fabrics#update", id: "1")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/fabrics/1").to route_to("fabrics#destroy", id: "1")
+    end
+  end
+end
