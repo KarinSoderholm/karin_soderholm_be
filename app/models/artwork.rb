c5b532f377938f465fd1b65f4e5e6d15be8dd243
George Soderholm <georgesoderholm@gmail.com>
2021-04-16 15:26:38 -0600

    Add many to many relationship with artshow_artworks

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,7 @@
 class Artwork < ApplicationRecord
+  has_many :artshows, through: :artshow_artworks
+  has_many :materials
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :image, presence: true
