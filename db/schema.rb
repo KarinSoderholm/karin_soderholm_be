765460c7cf02b6bcbf4a4109d66c76ecd1a74e96
George Soderholm <georgesoderholm@gmail.com>
2021-04-20 21:19:02 -0600

    Add tables to the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_032601) do
+ActiveRecord::Schema.define(version: 2021_04_21_015523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(version: 2021_04_16_032601) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "emails", force: :cascade do |t|
+    t.string "email_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
+
   create_table "fabrics", force: :cascade do |t|
     t.string "name"
     t.string "category"
@@ -124,6 +131,19 @@ ActiveRecord::Schema.define(version: 2021_04_16_032601) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["classroom_id"], name: "index_tools_on_classroom_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "artist_statements", "artworks"
