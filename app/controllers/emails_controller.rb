d88eb202f7327caefa0a29a2416cac1e78648f10
George Soderholm <georgesoderholm@gmail.com>
2021-04-21 22:23:00 -0600

    Add ability to add emails to the Email table

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,6 @@
 class EmailsController < ApplicationController
+  def new
+  end
 
   def create
     # binding.pry
@@ -18,11 +20,12 @@ class EmailsController < ApplicationController
       #     render :email, locals: { email: params }
       #   end
       # end
-      # flash[:notice] = "Welcome to Karin's Community. We will be in touch soon!"
-      redirect_to '/'
+      flash[:notice] = "Welcome to Karin's Community. We will be in touch soon!"
+      redirect_to welcome_index_path
     else
       # generate_flash(@email)
-      # flash[:notice] = 'You have already joined our community. Thank you for being apart of this already!'
+      flash.now[:notice] = 'You have already joined our community. Thank you for being apart of this already!'
+      # flash.now[:notice] = "Email #{generate_flash(@email)}! That means you are already apart of this community! Thank you!"
       # flash[:notice] = 'Something went wrong, please re-enter your information.'
       # respond_to do |format|
       #   format.html do
@@ -34,12 +37,23 @@ class EmailsController < ApplicationController
     end
   end
 
-  def flash
+  def show
 
   end
 
-  def show
+  def edit
+    @email = Email.find(params[:id])
+  end
 
+  def update
+    @email = Email.find(params[:id])
+    if @email.update(email_params)
+      flash[:notice] = 'Email has been updated!'
+      redirect_to admin_emails_path
+    else
+      flash[:alert] = 'There was a problem updating the email. Please make sure both fields are filled out!'
+      render :edit
+    end
   end
   private
   def email_params
