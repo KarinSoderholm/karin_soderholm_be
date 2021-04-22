785362517e682cf38517e70aa2792db2bfcfd6ce
George Soderholm <georgesoderholm@gmail.com>
2021-04-21 22:23:21 -0600

    Remove unwanted methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def self.format_date(date)
-    binding.pry
-  end
-
-  def self.format_time(time)
-    binding.pry
+  
+  def full_address
+    "#{self.address}, #{self.city}, #{self.state}, #{self.zip}"
   end
 end
