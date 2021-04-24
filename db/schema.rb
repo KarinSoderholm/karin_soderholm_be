cd710dfb593e4d9ef05eb87ea7de5902e9b055c7
George Soderholm <georgesoderholm@gmail.com>
2021-04-24 14:51:58 -0600

    Add items to artwork

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_000343) do
+ActiveRecord::Schema.define(version: 2021_04_24_181820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_04_23_000343) do
     t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "images", default: [], array: true
   end
 
   create_table "classrooms", force: :cascade do |t|
