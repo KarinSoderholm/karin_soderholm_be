95deac48e8fb0443b803afde163457178a40ba47
George Soderholm <georgesoderholm@gmail.com>
2021-04-24 14:59:34 -0600

    Add a .now to a flash[:notice] with a render

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       login_redirect(user)
     else
-      flash[:notice] = 'Your email or password was incorrect!'
+      flash.now[:notice] = 'Your email or password was incorrect!'
       render :new
     end
   end
