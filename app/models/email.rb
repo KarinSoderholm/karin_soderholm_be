e16ca0d32fb83c10f87decfba9aeb4248b032ccb
George Soderholm <georgesoderholm@gmail.com>
2021-04-25 13:47:22 -0600

    Add a method to find all the emails in the emails_table

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,4 +4,16 @@ class Email < ApplicationRecord
                 :uniqueness => true
                 # :email => true
   validates :name, :presence => true
+
+  def self.all_emails(current_user)
+    if current_user
+      emails = Email.all
+      email_addresses = emails.map do |email|
+        email.email_address
+      end
+      email_addresses.include?(current_user.email)
+    else
+      return false
+    end
+  end
 end
