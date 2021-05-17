edc0d9836edcc4156ee1e1acd585c0e500abab8d
George Soderholm <georgesoderholm@gmail.com>
2021-05-17 11:14:24 -0600

    Remove pry

--- app/controllers/admin/classrooms_controller.rb
+++ app/controllers/admin/classrooms_controller.rb
@@ -14,24 +14,20 @@ class Admin::ClassroomsController < Admin::BaseController
         end
       end
       @classroom = Classroom.new
-      binding.pry
+
       if @classroom.save
         redirect_to admin_classrooms_path, notice: "Classroom Data Imported Successfully!"
       else
-        binding.pry
         redirect_to admin_classrooms_path, alert: "Classroom Data Could not be imported. Please try again"
       end
     end
 
   end
 
-  # GET /classrooms
   def index
     @classrooms = Classroom.all
-    # render json: @classrooms
   end
 
-  # GET /classrooms/1
   def show
   end
 
@@ -40,40 +36,36 @@ class Admin::ClassroomsController < Admin::BaseController
     @requirements = Requirement.all
     @classrooms = Classroom.all
   end
-  # POST /classrooms
+
   def create
     classroom = Classroom.new(classroom_params)
     if classroom.save
       flash[:success] = 'Congrats! A new Workshop was created!'
       redirect_to admin_classrooms_path
-      # render json: @classroom, status: :created, location: @classroom
     else
       flash[:message] = 'All fields must be filled out. There is a missing field. Try again'
       render :new
-      # render json: @classroom.errors, status: :unprocessable_entity
     end
   end
 
   def edit
     @classroom = Classroom.find(params[:id])
   end
-  # PATCH/PUT /classrooms/1
+
   def update
     if @classroom.update(classroom_params)
       flash[:success] = 'Congrats! The Workshop was updated!'
       redirect_to admin_classrooms_path
-      # render json: @classroom
     else
       flash[:message] = 'All fields must be filled out. There is a missing field. Try again'
       render :edit
-      # render json: @classroom.errors, status: :unprocessable_entity
     end
   end
 
   def alert
     @classroom = Classroom.find(params[:classroom_id])
   end
-  # DELETE /classrooms/1
+
   def destroy
     @classroom = Classroom.find(params[:id])
     if @classroom.destroy
@@ -83,12 +75,10 @@ class Admin::ClassroomsController < Admin::BaseController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_classroom
       @classroom = Classroom.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def classroom_params
       if !params[:classroom].nil?
         params.require(:classroom).permit(:name, :description, :image, :date, :time, :location, :requirements, :tools_needed, :cost, :active, :photo)
