3ca1605d9274b18d0fa9887498ab2f3037bd1bf0
George Soderholm <georgesoderholm@gmail.com>
2021-05-27 09:45:21 -0600

    Add enum to Contact to track if Admin has seen/responded to the email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,8 @@ class Contact < ApplicationRecord
   validates :subject, presence: true
   validates :customer_email_address, presence: true
   validates :message, presence: true
+
+  # enum message_status: { read: 'read', responded: 'responeded', follow_up: 'follow_up' }, _prefix: :message_status
+  enum message_status: { unread: 'unread', read: 'read', responded: 'responeded', follow_up: 'follow_up' }, _prefix: :message_status
+  # enum message_status: [:unread, :read, :responded, :follow_up]
 end
