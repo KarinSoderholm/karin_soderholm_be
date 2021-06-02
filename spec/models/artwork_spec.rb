f492c9dc76cd09d6d78aae433d82b7b0a157ac88
George Soderholm <georgesoderholm@gmail.com>
2021-06-02 16:43:21 -0600

    Add method testing for Artwork

--- spec/models/artwork_spec.rb
+++ spec/models/artwork_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Artwork, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
-    it { should validate_presence_of :image }
+    # it { should validate_presence_of :image }
     it { should validate_presence_of :create_date }
     it { should validate_presence_of(:sell_date).allow_nil }
     it { should validate_presence_of :cost }
@@ -16,5 +16,57 @@ RSpec.describe Artwork, type: :model do
     it { should have_many :artshow_artworks }
     it { should have_many(:artshows).through :artshow_artworks }
     it { should have_one :artist_statement }
+    it { should have_many :order_artworks }
+    it { should have_many(:orders).through :order_artworks }
+    it { should have_many :artwork_collections }
+    it { should have_many(:collections).through :artwork_collections }
+  end
+
+  describe 'instance methods' do
+    before :each do
+      @niwot = Artwork.create!(name: 'Niwot',
+                              description: 'A map of Niwot',
+                              create_date: '2021-05-18',
+                              cost: 125.00,
+                              available: true)
+    end
+
+    it '.availability?' do
+      expect(@niwot.availability?).to eq('available!')
+      expect(@niwot.availability?).to_not eq('unavailable')
+    end
+
+  end
+
+  describe 'class methods' do
+    before :each do
+      @niwot = Artwork.create!(name: 'Niwot',
+                              description: 'A map of Niwot',
+                              create_date: '2021-05-18',
+                              cost: 125.00,
+                              available: true)
+      @cape_town = Artwork.create!(name: 'Cape Town',
+                              description: 'A map of Cape Town',
+                              create_date: '2021-05-18',
+                              cost: 125.00,
+                              available: false)
+      @brussels = Artwork.create!(name: 'Brussels',
+                              description: 'A map of Brussels',
+                              create_date: '2021-05-18',
+                              cost: 125.00,
+                              available: false)
+    end
+
+    it '#total_sold' do
+      expect(Artwork.total_sold).to eq 2
+      expect(Artwork.total_sold).to_not eq 0
+      expect(Artwork.total_sold).to_not eq 1
+    end
+
+    it '#total_available' do
+      expect(Artwork.total_available).to eq 1
+      expect(Artwork.total_available).to_not eq 0
+      expect(Artwork.total_available).to_not eq 2
+    end
   end
 end
