93a4e42e7e87e794269bd76320eee756e8084dc5
George Soderholm <georgesoderholm@gmail.com>
2021-06-17 20:24:00 -0600

    Add methods to deal with adding and deleting of images attached with ActiveStorage

--- app/controllers/admin/clothings_controller.rb
+++ app/controllers/admin/clothings_controller.rb
@@ -25,11 +25,13 @@ class Admin::ClothingsController < Admin::BaseController
 
   def create
     @clothing = Clothing.new(clothing_params)
-
+    binding.pry
     if @clothing.save
+      binding.pry
       flash[:success] = "Hooray! You have successfully added a Garment to the Collection!"
       redirect_to admin_clothings_path
     else
+      binding.pry
       flash.now[:alert] = "Please check to make sure the fields are all filled in properly. Try again"
       render :new
     end
@@ -38,9 +40,11 @@ class Admin::ClothingsController < Admin::BaseController
   def edit
     @clothings = Clothing.all
     @clothing = Clothing.find(params[:id])
+
   end
 
   def update
+    @clothings = Clothing.all
     if @clothing.update(clothing_params)
       flash[:success] = "Hooray! You have successfully updated your Garment!"
       redirect_to admin_clothings_path
@@ -54,7 +58,6 @@ class Admin::ClothingsController < Admin::BaseController
     @clothing = Clothing.find(params[:clothing_id])
   end
 
-  # DELETE /clothings/1
   def destroy
     if @clothing.destroy
       flash[:notice] = "You have successfully destroyed your Garment from the Collection"
@@ -62,23 +65,58 @@ class Admin::ClothingsController < Admin::BaseController
     end
   end
 
+  def add_photos
+    @clothing = Clothing.find(params[:clothing_id])
+  end
+
+  def photo_update
+    @clothing = Clothing.find(clothing_photos_params[:clothing_id])
+    if clothing_photos_params[:photo]
+      @clothing.photos.attach(clothing_photos_params[:photo])
+      flash[:success] = "A photo was successfully added to #{@clothing.name}"
+      redirect_to admin_clothing_path(@clothing)
+    else
+      flash.now[:error] = 'Please try again. Unable to process that request'
+      render :add_photos
+    end
+  end
+
+  def remove_photo
+    clothing = Clothing.find(params[:clothing_id])
+    if clothing
+      clothing.photos.find(params[:photo_id]).purge
+      flash[:success] = "A photo was successfully deleted from #{clothing.name}"
+      redirect_to admin_clothing_path(clothing)
+    else
+      flash.now[:error] = 'Please try again. Unable to process that request'
+      render :show
+    end
+  end
   private
     def set_clothing
       @clothing = Clothing.find(params[:id])
     end
 
+    def clothing_photos_params
+      hash = {}
+      hash[:clothing_id] = params[:clothing_id].to_i
+      hash[:photo] = params[:clothing][:photos]
+      return hash
+    end
+
     def clothing_params
       if !params[:clothing].nil?
-        params.require(:clothing).permit(:name, :description, :image, :fabric, :url, :category, :available, :pattern_name, :origin_date, :pattern_cost, :cost, images: [])
+        params.require(:clothing).permit(:name, :description, :fabric, :url, :category, :available, :pattern_name, :origin_date, :pattern_cost, :cost,  :image, photos: [])
       else
         hash = {}
         hash[:name] = params[:name]
         hash[:description] = params[:description]
-        hash[:image] = params[:image]
         hash[:category] = params[:category]
         hash[:available] = params[:available]
         hash[:origin_date] = params[:origin_date]
         hash[:cost] = params[:cost]
+        hash[:image] = params[:image]
+        hash[:photos] = params[:photos]
         return hash
       end
     end
