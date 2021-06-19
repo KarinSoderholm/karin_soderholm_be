ded94e2cba11c84248937dd3a7a53b7fac6ec897
George Soderholm <georgesoderholm@gmail.com>
2021-06-18 20:37:06 -0600

    Add relationship between user and cv

--- app/models/cv.rb
+++ app/models/cv.rb
@@ -1,7 +1,20 @@
-class Contact < ApplicationRecord
+class Cv < ApplicationRecord
   belongs_to :user
-  
+
   validates :name, presence: true
 
   has_one_attached :cv_file
+
+  def set_file(params)
+    if params[:cv][:cv_file]
+      self.cv_file.attach(params[:cv][:cv_file])
+    end
+  end
+
+  def delete_file(params)
+    binding.pry
+    if self.cv_file.attached?
+      self.cv_file.purge
+    end
+  end
 end
