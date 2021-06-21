9e08d4e751b44b309a7f3ce2d8a75a316084e7f1
George Soderholm <georgesoderholm@gmail.com>
2021-06-21 09:09:54 -0600

    Add FAQ model

--- app/models/frequently_asked_question.rb
+++ app/models/frequently_asked_question.rb
@@ -1,4 +1,8 @@
 class FrequentlyAskedQuestion < ApplicationRecord
   validates :question, presence: true
   validates :answer, presence: true
+
+  def self.total_frequently_asked_questions
+    FrequentlyAskedQuestion.all.count
+  end
 end
