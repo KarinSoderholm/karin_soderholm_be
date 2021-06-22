f48a951b135bf5a611f9c3a0dbfe370e16cb2a41
George Soderholm <georgesoderholm@gmail.com>
2021-06-21 22:14:04 -0600

    Update styling

--- app/controllers/admin/art/artwork_statements_controller.rb
+++ app/controllers/admin/art/artwork_statements_controller.rb
@@ -1,23 +1,21 @@
 class Admin::Art::ArtworkStatementsController < Admin::BaseController
-  before_action :set_artwork_statement, only: [:show, :edit, :update, :destroy]
+  before_action :set_artwork_statement, only: [ :edit, :update, :destroy]
+  before_action :set_artwork, only: [:show, :new, :create, :edit, :update, :destroy]
   def index
     @artwork_statements = ArtworkStatement.all
   end
 
   def show
-    @artwork = Artwork.find(params[:artwork_id])
+    @artwork_statement = ArtworkStatement.find(params[:id])
   end
 
   def new
-    @artwork = Artwork.find(params[:artwork_id])
   end
 
   def create
-    @artwork = Artwork.find(params[:artwork_id])
     @artwork_statement = ArtworkStatement.new(artwork_statement_params)
-    artwork = Artwork.find(artwork_statement_params[:artwork_id])
     if @artwork_statement.save
-      flash[:success] = "Hooray, you have saved a Statement to #{artwork.name}"
+      flash[:success] = "Hooray, you have saved a Statement to #{@artwork.name}"
       redirect_to "/admin/art/artwork_statement/#{@artwork_statement.id}"
     else
       flash.now[:error] = "Please make sure the field is filled out. Please try again"
@@ -26,14 +24,12 @@ class Admin::Art::ArtworkStatementsController < Admin::BaseController
   end
 
   def edit
-    @artwork = Artwork.find(artwork_statement_params[:artwork_id])
   end
 
   def update
-    artwork = Artwork.find(artwork_statement_params[:artwork_id])
     if @artwork_statement.update(artwork_statement_params)
       flash[:success] = "Hooray, you have updated your Artwork Statement"
-      redirect_to "/admin/art/#{artwork.id}/artwork_statement/#{@artwork_statement.id}"
+      redirect_to "/admin/art/#{@artwork.id}/artwork_statement/#{@artwork_statement.id}"
     else
       flash.now[:error] = "Please make sure the field is filled out. Please try again"
       render :edit
@@ -56,8 +52,12 @@ class Admin::Art::ArtworkStatementsController < Admin::BaseController
       @artwork_statement = ArtworkStatement.find(params[:id])
     end
 
+    def set_artwork
+      @artwork = Artwork.find(params[:artwork_id])
+    end
+
     def artwork_statement_params
-      if params[:artwork]
+      if params[:artwork_statement]
         params.require(:artwork_statement).permit(:statement, :artwork_id)
       else
         hash = {}
