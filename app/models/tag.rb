830479e330af495faff2059966955ae4e7bdb58e
George Soderholm <georgesoderholm@gmail.com>
2021-06-24 21:18:13 -0600

    Add Tag and TagPost Model with validations and relationships

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   has_many :tag_posts, dependent: :destroy
-  has_many :blogs, through: :tag_posts, dependent: :destroy
+  has_many :blog_posts, through: :tag_posts, dependent: :destroy
+
+  def self.find_names
+    Tag.pluck(:title)
+  end
 end
--- app/models/tag_post.rb
+++ app/models/tag_post.rb
@@ -1,4 +1,4 @@
 class TagPost < ApplicationRecord
   belongs_to :tag
-  belongs_to :post
+  belongs_to :blog_post
 end
