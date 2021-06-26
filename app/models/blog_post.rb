7c26a704d2bd8e805f67cfb650ac643f84ef4df4
George Soderholm <georgesoderholm@gmail.com>
2021-06-25 23:20:54 -0600

    Add methods to use ActiveRecord to query the DB for data pretaining to BlogPosts

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -23,11 +23,31 @@ class BlogPost < ApplicationRecord
   end
 
   def self.find_by_month
-    binding.pry
+    self.select("date_trunc('day', created_at)")
+      .group("date_trunc('day', created_at)")
+      .pluck("date_trunc('day', created_at)")
+  end
+
+  def self.format_monthly_blog_date
+    # send this to the BlogPostsController to have access to the month and year
+    find_by_month.uniq.map do |month_group|
+      [month_group.strftime("%B %Y"), month_group.strftime("%m"), month_group.strftime("%Y")]
+    end
+  end
+
+  def self.format_monthly_blog
+    # BlogPost.select('blog_posts.*').group("date_trunc('day', created_at)", :id).pluck('blog_posts.*')
+    # self.find_by_month.uniq.map do |month_group|
+      # month_group.strftime("%B %Y")
+    # end
+    blog_ids_by_date = {}
+    BlogPost.select(:created_at).group(:created_at).pluck(:created_at).map do |date|
+      blog_ids_by_date[(BlogPost.find_by(created_at: date)).id] = [date.strftime("%m"), date.strftime("%Y")]
+    end
+    return blog_ids_by_date
   end
 
   def self.find_blogs_by_tag(tag_id)
-    # binding.pry
     BlogPost.joins(:tags).where(tags: {id: tag_id})
   end
 end
