820c6dabddca4cf73b4c8a163de5ae2b2db2f4a5
George Soderholm <georgesoderholm@gmail.com>
2021-06-25 23:21:31 -0600

    Add method #monthly_blogs

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -11,4 +11,15 @@ class BlogPostsController < ApplicationController
     @blog_posts = BlogPost.find_blogs_by_tag(params[:tag_id])
     @tag = Tag.find(params[:tag_id])
   end
+
+  def monthly_blogs
+    posts = BlogPost.format_monthly_blog
+    @blog_posts = []
+    posts.map do |blog_id, date_array|
+      if date_array[0] == params[:month_id] && date_array[1] == params[:year_id]
+        @blog_posts << BlogPost.find(blog_id)
+      end
+    end
+    @monthly_blog_title = @blog_posts.first.created_at.strftime("%B %Y")
+  end
 end
