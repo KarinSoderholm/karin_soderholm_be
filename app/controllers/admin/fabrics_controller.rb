6a3dc92f006be4b1a9202285d1ad1c1f618c1f95
George Soderholm <georgesoderholm@gmail.com>
2021-06-26 20:03:08 -0600

    Remove a mis-spelling to help deployment

--- app/controllers/admin/fabrics_controller.rb
+++ app/controllers/admin/fabrics_controller.rb
@@ -1,21 +1,16 @@
-# class FabricsController < ApplicationController
 class Admin::FabricsController < Admin::BaseController
-  requrie 'csv'
   before_action :set_fabric, only: [:show, :update, :destroy]
 
-  # GET /fabrics
   def index
     @fabrics = Fabric.all
 
     render json: @fabrics
   end
 
-  # GET /fabrics/1
   def show
     render json: @fabric
   end
 
-  # POST /fabrics
   def create
     @fabric = Fabric.new(fabric_params)
 
@@ -26,7 +21,6 @@ class Admin::FabricsController < Admin::BaseController
     end
   end
 
-  # PATCH/PUT /fabrics/1
   def update
     if @fabric.update(fabric_params)
       render json: @fabric
@@ -35,18 +29,15 @@ class Admin::FabricsController < Admin::BaseController
     end
   end
 
-  # DELETE /fabrics/1
   def destroy
     @fabric.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_fabric
       @fabric = Fabric.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def fabric_params
       params.require(:fabric).permit(:name, :category, :clothing_id)
     end
