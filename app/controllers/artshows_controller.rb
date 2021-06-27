ce29825d54f58807336a2b65db275174099bf82e
George Soderholm <georgesoderholm@gmail.com>
2021-06-27 03:04:09 -0600

    Add an end

--- app/controllers/artshows_controller.rb
+++ app/controllers/artshows_controller.rb
@@ -1,19 +1,13 @@
 class ArtshowsController < ApplicationController
   before_action :set_artshow, only: [:show, :update, :destroy]
 
-  # GET /artshows
   def index
     @artshows = Artshow.all
-
-    # render json: @artshows
   end
 
-  # GET /artshows/1
   def show
-    # render json: @artshow
   end
 
-  # POST /artshows
   def create
     @artshow = Artshow.new(artshow_params)
 
@@ -22,18 +16,14 @@ class ArtshowsController < ApplicationController
     else
       render json: @artshow.errors, status: :unprocessable_entity
     end
-  # end
+  end
 
-  # PATCH/PUT /artshows/1
   def update
     if @artshow.update(artshow_params)
-      # render json: @artshow
     else
-      # render json: @artshow.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /artshows/1
   def destroy
     if @artshow.destroy
 
@@ -43,12 +33,10 @@ class ArtshowsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_artshow
       @artshow = Artshow.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def artshow_params
       params.require(:artshow).permit(:name, :location, :start_date, :end_date, :entry_fee)
     end
